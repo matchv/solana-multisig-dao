f83f0e3be6e70dabd034cf8be003c3024fe7daf3
zkppairing <provoip@gmail.com>
2023-03-01 07:42:54 -0500

    update odf-stake Anchor.tmol

--- onchain/odf-stake/tests/odf-stake.ts
+++ onchain/odf-stake/tests/odf-stake.ts
@@ -295,7 +295,7 @@ describe('Test Odf Stake', async () => {
           },
         })
       } catch (err) {
-        console.log('err.message:', err.message)
+        // console.log('err.message:', err.message)
         assert.ok(err.message.includes('not staked'))
       }
     })
